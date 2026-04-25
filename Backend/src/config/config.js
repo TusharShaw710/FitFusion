842f539c232ded91c0e823caa85528f217b057d3
Tushar Shaw <tusharshaw961@gmail.com>
2026-04-25 11:05:54 +0530

    feat: initialize environment configuration and validation schema for backend services

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -27,6 +27,11 @@ if(!process.env.IMAGE_KIT_SECRET){
     throw new Error("IMAGE_KIT_SECRET is not defined in environment variables")
 }
 
+if(!process.env.GOOGLE_REDIRECT_URI){
+    throw new Error("GOOGLE_REDIRECT_URI is not defined in environment variables")
+}
+
+
 
 
 export default {
@@ -37,4 +42,5 @@ export default {
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
     IMAGE_KIT_SECRET: process.env.IMAGE_KIT_SECRET,
+    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI
 };
